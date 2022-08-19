2a800011c319a15043324065a3f2e1247c7b08f5
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-19 17:26:15 +0900

    ㅍㅍ

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,18 @@ void main() {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World2',
-        ),
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/img/logo.png',
+          ),
+          const CircularProgressIndicator(
+            color: Colors.white,
+          ),
+        ],
       ),
     );
   }

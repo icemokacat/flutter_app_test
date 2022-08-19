fedb353e68cc4368d4c72b39a68d2b47a48ce323
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-19 13:41:19 +0900

    StatelessWidget 생성하기

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pinkAccent,
-        body: Center(
-          child: Text(
-              'Hello Center',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-              )
-          ),
-        )
-      ),
+    MaterialApp(
+      home: HomeScreen(),
     ),
   );
+}
+
+// widget
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World2',
+        ),
+      ),
+    );
+  }
 }

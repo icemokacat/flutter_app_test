680dcb87684a8e10df027ff9e70dcd0b35ce292f
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-22 16:52:38 +0900

    위젯 정렬 실습으로 복습하기

---
+++ lib/element/ColorBlock.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+class ColorBlock {
+
+  List<Widget> colorblocks = [
+    Container(
+      color: Colors.red,
+      width: 50.0,
+      height: 50.0,
+    ),
+    Container(
+      color: Colors.orange,
+      width: 50.0,
+      height: 50.0,
+    ),
+    Container(
+      color: Colors.yellow,
+      width: 50.0,
+      height: 50.0,
+    ),
+    Container(
+      color: Colors.green,
+      width: 50.0,
+      height: 50.0,
+    ),
+  ];
+
+
+
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../element/ColorBlock.dart';
+
 
 // widget
 class HomeScreen extends StatelessWidget {
@@ -12,35 +14,43 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
-          child: Row(
-            // MainAxisAligenment - 주축 정렬 (Column 일때 세로 row 일때 가로)
-            // spaceBetween : 위젯과 위젯의 사이가 동일하게 배치
-            // spaceEvenly  : 위젯을 같은 간격으로 배치하지만 끝과 끝에도 빈간격을 줌
-            // spaceAround  : spaceEvenly + 끝간격은 반으로 적용
-            mainAxisAlignment: MainAxisAlignment.start,
-            // stretch - 최대한으로 늘린다.
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+
             children: [
-              Container(
-                color: Colors.red,
-                width: 50.0,
-                height: 50.0,
+              // 1. spaceAround
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: ColorBlock().colorblocks
               ),
-              Container(
-                color: Colors.orange,
-                width: 50.0,
-                height: 50.0,
+              // 2. one center
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.orange,
+                    width: 50.0,
+                    height: 50.0,
+                  )
+                ],
               ),
-              Container(
-                color: Colors.yellow,
-                width: 50.0,
-                height: 50.0,
+              // 3. all right
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: ColorBlock().colorblocks
               ),
-              Container(
-                color: Colors.green,
-                width: 50.0,
-                height: 50.0,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.green,
+                    width: 50.0,
+                    height: 50.0,
+                  )
+                ],
               ),
             ],
           ),

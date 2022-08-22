e57a1c6fd2d4df94368c8b85087a2c678866bd66
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-22 18:02:51 +0900

    webview 초기 세팅

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,60 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-import '../element/ColorBlock.dart';
-
-
-// widget
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Container(
-          color: Colors.black,
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-
-            children: [
-              // 1. spaceAround
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: ColorBlock().colorblocks
-              ),
-              // 2. one center
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.orange,
-                    width: 50.0,
-                    height: 50.0,
-                  )
-                ],
-              ),
-              // 3. all right
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: ColorBlock().colorblocks
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.green,
-                    width: 50.0,
-                    height: 50.0,
-                  )
-                ],
-              ),
-            ],
-          ),
-        ),
+      body: WebView(
+        initialUrl: 'https://www.black-cat.online',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }

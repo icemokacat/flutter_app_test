53f4cee6937e9a8e4efe18961acac3082ff4148c
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-23 14:57:57 +0900

    IOS/Android 인터넷 권한 및 http 허용 web controller 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,16 +1,39 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController? controller;
+  final homeUrl = 'https://www.black-cat.online';
+
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text("Black-cat"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                // 기본적으로 http 로 접속하는 것을 막고 있음
+                if(controller == null){return;}
+                controller!.loadUrl(homeUrl);
+              },
+              icon: Icon(
+                Icons.home,
+              )
+          )
+        ],
+      ),
       body: WebView(
-        initialUrl: 'https://www.black-cat.online',
+        // 웹뷰가 만들어 졌을때 실행
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+        initialUrl: homeUrl,
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );

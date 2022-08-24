add9d6c255c80735767f538fcd08c2a97a6cadf6
이호진 <75923099+icemokacat@users.noreply.github.com>
2022-08-24 16:31:45 +0900

    텍스트 스타일 테마로 지정하여 저장하기

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,40 @@ import 'package:flutter_app_kot_swift/screen/home_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  runApp(MaterialApp(
-    localizationsDelegates: [
-      GlobalMaterialLocalizations.delegate,
-      GlobalCupertinoLocalizations.delegate,
-    ],
-    supportedLocales: [
-      const Locale('ko', 'KR'),
-      Locale('en', ''),
-    ],
-    home: HomeScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+        Locale('en', ''),
+      ],
+      theme: ThemeData(
+        fontFamily: 'sunflower',
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: Colors.white,
+            fontFamily: 'parisienne',
+            fontSize: 80.0,
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          )
+        )
+      ),
+      home: HomeScreen(),
+    )
+  );
 }
